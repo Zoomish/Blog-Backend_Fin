1b47857d299ab4f24e9f75daac504d90c02ddb4d
Zoom <egor147536987@gmail.com>
2024-02-19 15:43:44 +0300

    Changed `User.create` to `new User` and `save` (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const signup = async (req, res) => {
     }
     
     try{
-        const user = await new User.create({username, email, password});
+        const user = new User({username, email, password});
         await user.save();
         res.status(201).json(user)
     }catch(err){
