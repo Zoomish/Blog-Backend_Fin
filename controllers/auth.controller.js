703e3e97e7e06fa7d9469bcb1ee780e7b774372c
Zoom <egor147536987@gmail.com>
2024-02-19 15:42:56 +0300

    Update import path for user model. (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
     const {username, email, password} = req.body;
