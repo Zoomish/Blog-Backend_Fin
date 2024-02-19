236d0bffe32d87d603f1e16faccea8f652869813
Zoom <egor147536987@gmail.com>
2024-02-19 15:22:42 +0300

    Replace the default route with a new route that calls the test function from the user controller. (generated by blackbox.ai )

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({message: 'Hello World'})
-})
+router.get('/', test)
 
 export default router
