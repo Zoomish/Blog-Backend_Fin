1bd61852ae63f3ed9dc6ace5ce5530d8e9c247f8
Zoom <egor147536987@gmail.com>
2024-02-19 13:20:07 +0300

    Update package.json and package-lock.json by adding mongoose as a dependency. (generated by blackbox.ai )

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose'
 
+mongoose.connect('mongodb+srv://admin:QXmYtmxGgbF2Q6rx@cluster0.lebi7tr.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>console.log('Connected to MongoDB'))
 const app = express();
 
 app.listen(3000, () => {
