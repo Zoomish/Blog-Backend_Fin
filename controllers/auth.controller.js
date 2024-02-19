ad5e945f416da6bf62cd401e88b28d0615920e56
Zoom <egor147536987@gmail.com>
2024-02-19 15:50:00 +0300

    Added bcryptjs to hash passwords before saving to the database. (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,16 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
     const {username, email, password} = req.body;
     if (!username || !email || !password){
         return res.status(400).json({message: 'All fields are required'})
     }
+
+    const hashedPassword = await bcryptjs.hashSync(password, 12)
     
     try{
-        const user = new User({username, email, password});
+        const user = new User({username, email, hashedPassword});
         await user.save();
         res.status(201).json(user)
         User = mongoose.model('User', userSchema)
