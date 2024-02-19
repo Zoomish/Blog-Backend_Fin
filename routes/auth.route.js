5a0986b4796729178870596a903d7fa8201d75eb
Zoom <egor147536987@gmail.com>
2024-02-19 15:27:06 +0300

    Added login form route. (generated by blackbox.ai )

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/signup', signup)
+router.post('/signup', signup);
+router.get('/login', loginForm);
 
 export default router
