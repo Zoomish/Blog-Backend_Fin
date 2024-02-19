e3289c16e717153bbef1094926cc58124defa25c
Zoom <egor147536987@gmail.com>
2024-02-19 15:47:40 +0300

    Update User model (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,9 +8,9 @@ export const signup = async (req, res) => {
     
     try{
         const user = new User({username, email, password});
-        console.log(user)
         await user.save();
         res.status(201).json(user)
+        User = mongoose.model('User', userSchema)
     }catch(err){
         res.status(409).json({message: err.message})
     }
