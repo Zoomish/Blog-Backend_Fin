d7964bc77d60ef4fe56c501ca9412e4a0424bda6
Zoom <egor147536987@gmail.com>
2024-02-24 19:52:42 +0300

    Update the response to include the user object in the successful sign in response. (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const signin = async (req, res, next) => {
             res.status(200).cookie('access_token', token, {
                 expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 10),
                 httpOnly: true
-            }).json('SignIn Succsessfull')
+            }).json(user)
         } else {
             res.status(401).json({
                 success: false,
