3970ed2eb82ee2304f6e000d3a6cb9ad02fd114e
Zoom <egor147536987@gmail.com>
2024-02-24 20:00:24 +0300

    Redact user password from response (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -41,10 +41,13 @@ export const signin = async (req, res, next) => {
                     expiresIn: '10d'
                 }
             )
+
+            const { password: pass, ...rest } = user._doc
+
             res.status(200).cookie('access_token', token, {
                 expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 10),
                 httpOnly: true
-            }).json(user)
+            }).json(rest)
         } else {
             res.status(401).json({
                 success: false,
