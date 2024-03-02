481a88448b5727896e51e7f7d51c9349ba09d753
Zoom <egor147536987@gmail.com>
2024-03-02 18:09:32 +0300

    Add JWT token generation and cookie setting after successful Google OAuth login. (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -90,6 +90,20 @@ export const google = async (req, res, next) => {
                 profilePicture: googlePhotoUrl
             })
             await newUser.save();
+            const { password, ...rest } = newUser._doc
+            const token = jwt.sign(
+                {
+                    id: newUser._id
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: '10d'
+                }
+            )
+            res.status(200).cookie('access_token', token, {
+                expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 10),
+                httpOnly: true
+            }).json(rest)
         }
     } catch (error) {
         next(error)
