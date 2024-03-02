5a48e2a1461ce2c315966273efe8c6489099c943
Zoom <egor147536987@gmail.com>
2024-03-02 18:01:13 +0300

    Changed username generation to be a combination of the user's name and a random string. (generated by blackbox.ai )

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -83,10 +83,7 @@ export const google = async (req, res, next) => {
         else {
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const newUser = new User({
-                username: name,
-                email,
-                image: googlePhotoUrl,
-                password: crypto.randomBytes(20).toString('hex')
+                username: name.toLowerCase().split(' ').join('') + Math.random().toString(36).slice(-8),
             })
         }
     } catch (error) {
