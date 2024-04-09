6449a1837f466bbe5215955cc286eba3a36fdfe9
Zoom <egor147536987@gmail.com>
2024-04-09 20:31:49 +0300

    "Added updateUser function to user.route.js"
    Explanation:
    
    The code diff shows that a new function, `updateUser`, has been imported into the `user.route.js` file from the `user.controller.js` file. However, the `test` function has also been retained in the import statement. Therefore, the commit message should accurately reflect this change without making any assumptions about why it was made.

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { test } from '../controllers/user.controller.js'
+import { test, updateUser } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
