2ac2a6722b0dcb7a4992c5a5b4e0884ad260c923
Zoom <egor147536987@gmail.com>
2024-04-09 20:32:05 +0300

    "Convert user controller's updateUser function to accept next middleware function as parameter"
    Explanation: The updateUser function in the user controller has been modified to accept an additional parameter, 'next', which is used to reference the next middleware function in the application's request-response cycle.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,6 @@ export const test = (req, res) => {
     res.json({ message: 'Hello World' })
 }
 
-export const updateUser = (req, res) => {
+export const updateUser = (req, res, next) => {
     res.json({ message: 'Update User' })
 }
