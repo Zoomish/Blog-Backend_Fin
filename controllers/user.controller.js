ce7967f899cd6074649582325294e18a3db9e1a9
Zoom <egor147536987@gmail.com>
2024-04-17 17:56:17 +0300

    "Log req.user instead of the entire req object in updateUser function"

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,6 @@ export const test = (req, res) => {
 }
 
 export const updateUser = (req, res, next) => {
-    console.log(req);
+    console.log(req.user);
     res.json({ message: 'Update User' })
 }
