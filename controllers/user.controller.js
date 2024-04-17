a319f5792a7b47485e9f942e10eb985086f3d83f
Zoom <egor147536987@gmail.com>
2024-04-17 17:58:13 +0300

    "Add password length validation for user update"
    This commit adds a new validation to the user update function to check if the new password (if provided) is at least 6 characters long. If the password is less than 6 characters,

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,6 +6,11 @@ export const updateUser = (req, res, next) => {
     if (req.user.id !== req.params.id) {
         return next(errorHandler(403, 'You can update only your account'))
     }
+    if (req.body.params) {
+        if (req.body.password.lenght < 6) {
+            return next(errorHandler(400, 'Password must be at least 6 characters'))
+        }
+    }
     console.log(req.user);
     res.json({ message: 'Update User' })
 }
